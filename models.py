34309f4a009ae27c3f42152939d682dea887831c
Rio Dwi Putra P <dwiputra.rio@gmail.com>
2019-02-27 00:59:23 +0700

    Update and Union

--- models.py
+++ models.py
@@ -5,3 +5,10 @@ from wtforms.validators import DataRequired
 class UpdateForm(FlaskForm):
   update = StringField('UPDATE', validators=[DataRequired()])
   submit = SubmitField('Start')
+
+class UnionForm(FlaskForm):
+  select1 = StringField('Select 1', validators=[DataRequired()])
+  table1 = StringField('Table 1', validators=[DataRequired()])
+  select2 = StringField('Select 2', validators=[DataRequired()])
+  table2 = StringField('Table 2', validators=[DataRequired()])
+  submit = SubmitField('Start')
