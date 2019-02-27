f1fd5e5e591dbdb2b7ba94a1a71548d0f96e082b
Paskahlis Anjas <pjaswo1204@gmail.com>
2019-02-27 20:42:57 +0700

    return json

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
 from flask import Flask
 from flask import render_template
+from flask import jsonify
+from flask import request
+import json
+import re
 app = Flask(__name__)
+
+from db_manager.db_manager import Database
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/delete')
+@app.route('/delete', methods=['POST'])
 def delete():
-    return render_template('delete.html')
+    data = request.get_json()
+
+    db = Database()
+    db.execute_sql('SELECT * FROM salary')
+    salaries = db.fetch_all()
+
+    # db.execute_sql('SELECT * FROM title')
+    # titles = db.fetch_all()
+    # return render_template(
+    #     'delete.html',
+    #     salaries=salaries,
+    #     titles=titles
+    # )
+    return jsonify(salaries)
--- db_manager/delete_operation.py
+++ db_manager/delete_operation.py
@@ -1,6 +1,19 @@
 TABLE_NAMES = ['salary', 'title']
 
-def delete_record(table_name, **kwargs):
+def build_query(table_name, *args):
+    query = 'DELETE FROM %s WHERE' % table_name
+    iterator = 0
+    for arg in args:
+        if iterator % 2 == 0:
+            query += ' %s %s \'%s\'' % (arg['attr'], arg['opr'], arg['val'])
+        else:
+            query += ' ' + arg
+        iterator += 1
+    query += ';'
+    return query
+
+def delete_record(table_name, *args):
     if table_name not in TABLE_NAMES:
-        raise 'Invalid table name!'
-    query = 'DELETE '
+        raise 'Invalid table name: %s' % table_name
+    query = build_query(table_name, *args)
+    print(query)
